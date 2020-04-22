a5d44865942dc30a2c0f40a480e2ebfb9495fb38
Christian Santos <christian.santos@ibti.org.br>
2020-04-22 11:50:56 -0300

    add func para reconhecer padrao json

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,10 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(routes);
+
 app.listen(3333, () => {
   console.log('listening on port :3333 🎈🎈🎈🎈');
 });
